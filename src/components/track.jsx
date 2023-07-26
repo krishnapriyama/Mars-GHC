9d19cef4129ed4a7072f54e237a319a25158cb2b
Krishnapriya MA <krishnapriyama185@gmail.com>
2023-07-26 12:39:11 +0530

    first commit

--- src/components/track.jsx
+++ src/components/track.jsx
@@ -1,4 +1,4 @@
-import vector from "../assets/vector.png";
+import arrow from "../assets/vector.png";
 const Track = () => {
   return (
     <>
@@ -6,7 +6,7 @@ const Track = () => {
         <div className="w-[90%] bg-[#333] rounded-lg flex">
           <h1 className="text-white p-3 md:text-xl text-sm w-full">View all trackers</h1>
           <span className="flex justify-end items-center w-full p-5">
-            <img src={vector} alt="" />
+            <img src={arrow} alt="" />
           </span>
         </div>
       </div>
